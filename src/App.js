fc3bb37491803ecfba3c62bdc7b89c09b7f5328b
Brian Bycynski <high5bri@hotmail.com>
2017-12-12 10:56:03 -0500

    ejected to implement css modules

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import Person from './Person/Person'
+
 import './App.css'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Burger Builder2</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to
-                    reload.
-                </p>
+                <Person />
             </div>
         )
     }
