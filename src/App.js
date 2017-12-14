11b5f47da33bf27404de4f6f18bbc0fb02b216b6
Brian Bycynski <high5bri@hotmail.com>
2017-12-14 09:41:49 -0500

    just starting Burger Builder2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-import Person from './Person/Person'
-
-import './App.css'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Person />
+            <div>
+                <h1>Bdog</h1>
             </div>
         )
     }
